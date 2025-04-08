1a1fc9c5ec435f594aefe77870ea1f933e0ce4c3
Juan Ignacio Rodriguez de Leon <jileon@parcan.es>
2025-04-08 19:50:19 +0100

    Assets para asteroris y pong

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## Pong
+
+### Créditos
+
+- [Fuente de pong
+  extendida](https://fontstruct.com/fontstructions/show/2450791/pong-score-extended)
+  por [https://fontstruct.com/fontstructors/2414584/randomfontstructuser
+](randomfontstructuser).
--- pong/main.gd
+++ pong/main.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+const INITIAL_SPEED_AMPLITUDE: float = 400.0
+var debug: bool = true
+
 var score_player_one: int = 0
 var score_player_two: int = 0
-
-var speed_amplitude: float = 400.0
+var speed_amplitude: float = INITIAL_SPEED_AMPLITUDE
 var ball_speed: Vector2
 var pad_speed: int = 160
 
@@ -14,19 +16,19 @@ func _ready() -> void:
 	start_play()
 	
 func start_play():
-	print('start_play')
 	ball.position = Vector2(620, 350)
+	speed_amplitude = INITIAL_SPEED_AMPLITUDE
 	ball_speed = Vector2(620, randf_range(-350, 350)).normalized() * speed_amplitude
 	$Timer.start(1)
 
 func update_scores(to_player_one: int = 0, to_player_two: int = 0):
 	if to_player_one > 0:
 		score_player_one += to_player_one
-		$GUI/lbl_score_player_one.text = 'Score player one: ' + str(score_player_one)
+		$Scores/ScorePlayerOne.text = str(score_player_one)
 	if to_player_two > 0:
 		score_player_two += to_player_two
-		$GUI/lbl_score_player_two.text = 'Score player two: ' + str(score_player_two)
-	
+		$Scores/ScorePlayerTwo.text = str(score_player_two)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	ball.position += ball_speed * delta
@@ -38,32 +40,48 @@ func _process(delta: float) -> void:
 		$PlayerTwo.position.y -= pad_speed * delta
 	elif Input.is_action_pressed("player_two_down"):
 		$PlayerTwo.position.y += pad_speed * delta
+	if debug:
+		$GUI/ball_speed_x.text = 'ball_speed.x: ' + str(ball_speed.x)
+		$GUI/ball_speed_y.text = 'ball_speed.y: ' + str(ball_speed.y)
+		$GUI/lbl_speed_amplitude.text = 'Speed: ' + str(speed_amplitude)
+
+	if Input.is_action_pressed('debug'):
+		debug = not debug
+		$GUI.set_visible(debug)
 		
-	$GUI/ball_speed_x.text = 'ball_speed.x:' + str(ball_speed.x)
-	$GUI/ball_speed_y.text = 'ball_speed.y:' + str(ball_speed.y)
-	if Input.is_action_pressed('pause'):
-		get_tree().paused = not get_tree().paused
 	queue_redraw()
 	
 func _draw():
-	draw_line(
-		ball.position,
-		ball.position + ball_speed,
-		Color('#ffCCCC'), 
-		2,
-		)
+	if debug: 
+		draw_circle($PlayerTwo.position + Vector2(100, 0), 2, Color(.1, 1, .1))
+		draw_circle($PlayerOne.position + Vector2(-100, 0), 2, Color(1, .1, .1))
+		var arrow: Vector2 = ball.position + ball_speed
+		var orientation = ball_speed.normalized() * 22;
+		var head = [
+			ball.position + ball_speed,
+			ball.position + ball_speed - orientation.rotated(0.7854),
+			ball.position + ball_speed + orientation.rotated(2.562),
+			]
+		var godot_blue : Color = Color("478cbf")
+		# We pass the PackedVector2Array to draw the shape.1,5708
+		draw_polygon(head, [godot_blue])
+		draw_line(ball.position, arrow, godot_blue, 1)
 	
-func _on_pad_collision(area: Area2D) -> void:
+func _on_right_pad_collision(area: Area2D) -> void:
 	$AudioPlayer.play()
-	ball_speed.x = -ball_speed.x
-	print("Antes:", ball_speed)
-	ball_speed = ball_speed.rotated(randf_range(-0.3, 0.3))
-	print("Después:", ball_speed)
+	ball_bounce()
+	var origin: Vector2 =  $PlayerTwo.position + Vector2(100, 0)
+	ball_speed = origin.direction_to(ball.position) * speed_amplitude
+	
 
+func _on_left_pad_collision(area: Area2D) -> void:
+	$AudioPlayer.play()
+	ball_bounce()
+	var origin: Vector2 =  $PlayerOne.position + Vector2(-100, 0)
+	ball_speed = origin.direction_to(ball.position) * speed_amplitude
+	
+	
 func _on_vertical_exit(area: Area2D) -> void:
-	print("_on_vertical_exit", area)
-	print(area, area.name)
-	print("Colisión con la banda horizontal. Velocidad en y:", ball_speed.y)
 	ball_speed.y = -ball_speed.y
 	ball_speed.rotated(randf_range(-0.3, 0.3))
 
@@ -78,10 +96,10 @@ func _on_left_out_area_entered(area: Area2D) -> void:
 	start_play()
 
 
-func ball_bounce(node: Node) -> void:
+func ball_bounce() -> void:
 	ball_speed.x = -ball_speed.x
 
 
 func inc_speed() -> void:
-	speed_amplitude += 1
+	speed_amplitude *= 1.01
 	$GUI/lbl_speed_amplitude.text = 'Speed amplitud: ' + str(speed_amplitude)
