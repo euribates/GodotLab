a58739539a32326d1954067c5bbb91f539063d0b
Juan Ignacio Rodriguez de Leon <jileon@parcan.es>
2025-04-15 13:54:37 +0100

    Supermemory

---
+++ supermemory/main.gd
@@ -0,0 +1,34 @@
+extends Node3D
+
+var slot: Sprite3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	slot = get_node('Sprite3D')
+
+
+	
+func _process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed('ui_accept'):
+		print("slot.rotation: ", slot.rotation, -PI/2.0)
+		var rotation_a = Vector3(0, -PI, 0)
+		var rotation_b = Vector3(0, 0, 0)		
+		var tween = get_tree().create_tween()
+		tween.tween_property(slot, "modulate", Color(1, 0.75, 0.75, 1.0), 0.3).set_trans(Tween.TRANS_SINE)
+		# tween.tween_property(slot, "scale", Vector3(2.4, 2.4, 1), 0.7).set_trans(Tween.TRANS_BOUNCE)
+		var t : Transform3D = Transform3D(self.basis, Vector3(0, 0, 8))
+		tween.tween_property(slot, "transform", t, 2).set_trans(Tween.TRANS_ELASTIC)
+		tween.tween_property(slot, "rotation", rotation_a, 1).set_ease(Tween.EASE_OUT)
+		tween.tween_callback(print.bind(self.transform))
+		tween.tween_property(slot, "rotation", rotation_b, 1).set_ease(Tween.EASE_IN)
+		tween.tween_property(slot, "transform", Transform3D(), 2).set_trans(Tween.TRANS_ELASTIC)
+		tween.tween_callback(print.bind(self.transform))
+		# tween.tween_property(slot, "scale", Vector3(1, 1, 1), 0.7)
+		tween.tween_property(slot, "modulate", Color(1.0, 1.0, 1.0, 1.0), 0.3)
+		tween.tween_callback(print.bind(self.transform))
+
+		# tween.tween_property(slot, "rotation.y", PI, 2.5)
+		
+	# print(slot.rotation.y)
+	
