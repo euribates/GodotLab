9ce45f5489810a4f3631b172b0c99cafc2b9a340
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2025-04-17 23:53:52 +0100

    Pruebas con rotaciones

--- supermemory/main.gd
+++ supermemory/main.gd
@@ -1,32 +1,39 @@
 extends Node3D
 
 var slot: Sprite3D
+var texture: ImageTexture
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	slot = get_node('Sprite3D')
+	var image = Image.load_from_file("res://Images/Olrik.png")
+	texture = ImageTexture.create_from_image(image)
 
+func switch():
+	print("switch")
+	slot.texture = texture
 	
 func _process(delta: float) -> void:
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-		slot.texture = load("res://Images/Olrik.png")
+		switch()
+		
 	if Input.is_action_just_pressed('ui_accept'):
-		print("slot.rotation: ", slot.rotation, -PI/2.0)
-		var rotation_a = Vector3(0, -PI, 0)
+		var rotation_a = Vector3(0, PI/2.0, 0)
 		var rotation_b = Vector3(0, 0, 0)		
 		var tween = get_tree().create_tween()
-		tween.tween_property(slot, "modulate", Color(1, 0.75, 0.75, 1.0), 0.3).set_trans(Tween.TRANS_SINE)
+		# tween.tween_property(slot, "modulate", Color(1, 0.75, 0.75, 1.0), 0.3).set_trans(Tween.TRANS_SINE)
 		# tween.tween_property(slot, "scale", Vector3(2.4, 2.4, 1), 0.7).set_trans(Tween.TRANS_BOUNCE)
-		var t : Transform3D = Transform3D(self.basis, Vector3(0, 0, 15))
+		var t : Transform3D = Transform3D(self.basis, Vector3(0, 0, 12))
 		tween.tween_property(slot, "transform", t, 2).set_trans(Tween.TRANS_ELASTIC)
 		tween.tween_property(slot, "rotation", rotation_a, 1).set_ease(Tween.EASE_OUT)
+		
 		tween.tween_callback(print.bind(self.transform))
+		tween.tween_callback(switch)
 		tween.tween_property(slot, "rotation", rotation_b, 1).set_ease(Tween.EASE_IN)
-		tween.tween_property(slot, "transform", Transform3D(), 2).set_trans(Tween.TRANS_ELASTIC)
-		tween.tween_callback(print.bind(self.transform))
+		tween.tween_property(slot, "transform", Transform3D(), 1).set_trans(Tween.TRANS_ELASTIC)
 		# tween.tween_property(slot, "scale", Vector3(1, 1, 1), 0.7)
-		tween.tween_property(slot, "modulate", Color(1.0, 1.0, 1.0, 1.0), 0.3)
+		# tween.tween_property(slot, "modulate", Color(1.0, 1.0, 1.0, 1.0), 0.3)
 		tween.tween_callback(print.bind(self.transform))
 
 		# tween.tween_property(slot, "rotation.y", PI, 2.5)
