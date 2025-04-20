d7774bb3c68db30055481a005adfd6e9ca490cfe
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2025-04-20 02:16:58 +0100

    Rotaciones con quaterniones

--- supermemory/main.gd
+++ supermemory/main.gd
@@ -1,42 +1,64 @@
 extends Node3D
+	
+@export var character : Texture
 
-var slot: Sprite3D
+var t_fordward : Transform3D = Transform3D(self.basis, Vector3(0, 0, 12))
+var t_backward : Transform3D = Transform3D(self.basis, Vector3(0, 0, 0))
+	
+var chareacter_background : Image
+var chareacter_foreground : Image
+
 var texture: ImageTexture
+@onready var slot: Sprite3D = $Sprite3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	slot = get_node('Sprite3D')
-	var image = Image.load_from_file("res://Images/Olrik.png")
-	texture = ImageTexture.create_from_image(image)
+	chareacter_background = Image.load_from_file("res://Images/fondo-personaje.png")
+	chareacter_foreground = Image.load_from_file("res://Images/BlakeAndMortimer.png")
+	slot.texture = ImageTexture.create_from_image(chareacter_background)
 
 func switch():
-	print("switch")
-	slot.texture = texture
+	print("switch on")
+	var image = Image.load_from_file("res://Images/DoctorInfierno.png")
+	var tween = get_tree().create_tween()
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(slot, "transform", t_fordward, 0.4)
+	tween.tween_property(slot, 'quaternion', Quaternion(0, -0.7071068, 0, 0.7071068), 0.4)
+	tween.tween_property(slot, 'texture', ImageTexture.create_from_image(chareacter_foreground), 0)	
+	tween.tween_property(slot, 'quaternion', Quaternion(0, -1, 0, 0), 0.4)
+	tween.tween_interval(2.2)	
+	tween.tween_property(slot, 'quaternion', Quaternion(0, 0.7071068, 0, 0.7071068), 0.4)
+	tween.tween_property(slot, 'texture', ImageTexture.create_from_image(chareacter_background), 0)	
+	tween.tween_property(slot, 'quaternion', Quaternion(0, 0, 0, 1), 0.4)
+	tween.tween_property(slot, "transform", t_backward, 0.4)
+			
+func switch_on():
+	print("switch on")
+	var tween = get_tree().create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.tween_property(slot, "transform", t_fordward, 0.5)
+	tween.tween_property(slot, 'quaternion', Quaternion(0, -0.7071068, 0, 0.7071068), 0.25)
+	tween.tween_property(slot, 'texture', ImageTexture.create_from_image(chareacter_foreground), 0)	
+	tween.tween_property(slot, 'quaternion', Quaternion(0, -1, 0, 0), 0.25)		
+
+func switch_off():
+	var tween = get_tree().create_tween()
+	tween.set_trans(Tween.TRANS_QUAD)
+	print("switch off")
+	tween.tween_property(slot, 'quaternion', Quaternion(0, -0.7071068, 0, -0.7071068), 0.25)
+	tween.tween_property(slot, 'texture', ImageTexture.create_from_image(chareacter_background), 0)	
+	tween.tween_property(slot, 'quaternion', Quaternion(0, 0, 0, 1), 0.25)
+	tween.tween_property(slot, "transform", t_backward, 0.5)
 	
 func _process(delta: float) -> void:
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-		switch()
+		print("right")
+		switch_off()
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		print("left")
+		switch_on()
 		
 	if Input.is_action_just_pressed('ui_accept'):
-		var rotation_a = Vector3(0, PI/2.0, 0)
-		var rotation_b = Vector3(0, 0, 0)		
-		var tween = get_tree().create_tween()
-		# tween.tween_property(slot, "modulate", Color(1, 0.75, 0.75, 1.0), 0.3).set_trans(Tween.TRANS_SINE)
-		# tween.tween_property(slot, "scale", Vector3(2.4, 2.4, 1), 0.7).set_trans(Tween.TRANS_BOUNCE)
-		var t : Transform3D = Transform3D(self.basis, Vector3(0, 0, 12))
-		tween.tween_property(slot, "transform", t, 2).set_trans(Tween.TRANS_ELASTIC)
-		tween.tween_property(slot, "rotation", rotation_a, 1).set_ease(Tween.EASE_OUT)
+		switch()
 		
-		tween.tween_callback(print.bind(self.transform))
-		tween.tween_callback(switch)
-		tween.tween_property(slot, "rotation", rotation_b, 1).set_ease(Tween.EASE_IN)
-		tween.tween_property(slot, "transform", Transform3D(), 1).set_trans(Tween.TRANS_ELASTIC)
-		# tween.tween_property(slot, "scale", Vector3(1, 1, 1), 0.7)
-		# tween.tween_property(slot, "modulate", Color(1.0, 1.0, 1.0, 1.0), 0.3)
-		tween.tween_callback(print.bind(self.transform))
-
-		# tween.tween_property(slot, "rotation.y", PI, 2.5)
-		
-	# print(slot.rotation.y)
-	
