1ba6e31a19118aee0b099b973434e01a56d11b98
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2025-04-23 20:33:38 +0100

    Secuencia de cuenta atras (Pero un poco chapucero)

--- asteroids/menu.gd
+++ asteroids/menu.gd
@@ -1,44 +1,44 @@
 extends Control
 
-@onready var etiqueta : Sprite2D = $Sprite2D
+@onready var sprite_one : Sprite2D = $SpriteOne
+@onready var sprite_two : Sprite2D = $SpriteTwo
+@onready var sprite_three : Sprite2D = $SpriteThree
 
 
 func start_sequence():
-	etiqueta.visible = true
-	var tween := get_tree().create_tween()
+	var tween_3 := get_tree().create_tween()
+	tween_3.tween_callback(print.bind("3"))
+	tween_3.tween_property(sprite_three, "visible", true, 0)
+	tween_3.set_parallel(true)
+	tween_3.tween_property(sprite_three, "modulate", Color(255, 255, 255, 0), 1)
+	tween_3.tween_property(sprite_three, "scale", Vector2(4, 4), 1)
+	tween_3.chain().tween_property(sprite_three, "visible", false, 1)
 	
-	tween.tween_property(etiqueta, "scale", Vector2(4, 4), 1)
-	tween.parallel()
-	tween.tween_callback(print.bind("3"))
-	tween.parallel()
-	tween.tween_property(etiqueta, "modulate", Color(255, 255, 255, 0), 1)
-	tween.tween_property(etiqueta, "frame", 1, 0)
-	tween.tween_property(etiqueta, "scale", Vector2(1, 1), 0)
-	tween.tween_property(etiqueta, "modulate", Color(255, 255, 255, 255), 0)
-		
-	tween.tween_property(etiqueta, "frame", 2, 0)
-	tween.tween_property(etiqueta, "scale", Vector2(1, 1), 0)
-	tween.parallel()
-	tween.tween_property(etiqueta, "modulate", Color(255, 255, 255, 255), 0)
-
-	tween.tween_property(etiqueta, "frame", 1, 0)
-	tween.tween_property(etiqueta, "scale", Vector2(1, 1), 0)
-	tween.tween_property(etiqueta, "modulate", Color(255, 255, 255, 255), 0)
-
-	tween.tween_property(etiqueta, "frame", 3, 0)
-	tween.tween_property(etiqueta, "scale", Vector2(4, 4), 1)
-	tween.tween_property(etiqueta, "modulate", Color(255, 255, 255, 0), 1)
-	tween.set_parallel(false)
-
+	var tween_2 := get_tree().create_tween()
+	tween_2.tween_interval(1)
+	tween_2.tween_callback(print.bind("2"))
+	tween_2.tween_property(sprite_two, "visible", true, 0)
+	tween_2.set_parallel()
+	tween_2.tween_property(sprite_two, "scale", Vector2(4, 4), 1)
+	tween_2.tween_property(sprite_two, "modulate", Color(55, 255, 255, 0), 1)
+	tween_2.chain()
+	tween_2.tween_property(sprite_two, "visible", false, 0)
 	
-	tween.tween_interval(1)
-	tween.tween_callback(print.bind("2"))
-	tween.tween_interval(1)
-	tween.tween_callback(print.bind("1"))
-	tween.tween_interval(1)
-	tween.tween_callback(print.bind("Start!"))
+
+	var tween_1 := get_tree().create_tween()
+	tween_1.tween_interval(2)
+	tween_1.tween_callback(print.bind("1"))
+	tween_1.tween_property(sprite_one, "visible", true, 0)
+	tween_1.set_parallel()
+	tween_1.tween_property(sprite_one, "scale", Vector2(4, 4), 1)
+	tween_1.tween_property(sprite_one, "modulate", Color(55, 255, 255, 0), 1)
+	tween_1.chain()
+	tween_1.tween_property(sprite_one, "visible", false, 0)
+	tween_1.tween_callback(start_game)
+
+func start_game():
+	get_tree().change_scene_to_file("res://Main.tscn")
 	
 
 func _on_button_pressed() -> void:
-	print("OK")
 	start_sequence()
