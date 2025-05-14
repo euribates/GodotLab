03f0988b55d5312cd924238c5dd16a5c25b6d375
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2025-05-14 18:08:37 +0100

    Barbarian blasters: add base

--- barbarian_blasters/Enemies/enemy.gd
+++ barbarian_blasters/Enemies/enemy.gd
@@ -2,6 +2,13 @@ extends PathFollow3D
 
 @export var speed : float = 3.2
 
+@onready var base = get_tree().get_first_node_in_group("base")
+
+
 func _process(delta: float) -> void:
 	self.progress += speed * delta
+	if self.progress_ratio > 0.9:
+		print(self.progress_ratio)
+	if self.progress_ratio == 1.0:
+		base.take_damage()
 	
